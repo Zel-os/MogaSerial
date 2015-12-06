42d80c7ba1639fc14477e8d99708b4ee23a22cb9
Jake Montgomery <jmont@sonic.net>
2015-12-05 19:27:29 -0800

    Code base for second forum release, labeled 0.92
    Many code cleanups.
    Changed handler routine from active polling to listen mode.

--- MogaSerial/MogaSerial.cpp
+++ MogaSerial/MogaSerial.cpp
@@ -27,32 +27,10 @@ Revision History:
 
 
 // Link to ws2_32.lib
-#include <stdio.h>
-#include <stdint.h>
-#include <signal.h>
-#include <winsock2.h>
-#include <ws2bth.h>
-#include "public.h"
-#include "vjoyinterface.h"
- 
-typedef ULONGLONG BT_ADDR;
-
-#define     SENDMSG_LEN     5
-#define     RECVBUF_LEN   256
-#define     MOGABUF_LEN     8
-
-#define     FixAxis(c)      ((c<=0x80)?(c+=0x80):(c-=0x80))
-
-BOOL            KeepGoing = true;
-// Moga connection/status info.  Struct this up if multi-Moga support is ever added.
-UINT            vJoyInterface = 1;
-BT_ADDR         MogaAddr;
-SOCKET          MogaSocket = INVALID_SOCKET;
-unsigned char   MogaCID = 0x01;
-unsigned char   MogaState[MOGABUF_LEN];
+#include	"MogaSerial.h"
 
 
-BOOL FindMogaAddress()
+BOOL FindMogaAddress(MOGA_DATA *pMogaData)
 {
 	union {
 		CHAR buf[5000];
@@ -101,7 +79,7 @@ BOOL FindMogaAddress()
 		fgets(buf, sizeof(buf), stdin);
 		retVal = sscanf_s(buf, "%d", &j);
 	} while (j < 1 || j > i || retVal!=1);
-	MogaAddr = btAddrList[j];
+	pMogaData->Addr = btAddrList[j];
 
 	return 1;
 }
@@ -120,18 +98,19 @@ BOOL FindMogaAddress()
 //   101 - poll command response, analog triggers  (14b response)
 //   102 - listen mode status update, analog triggers  (14b response)
 // Oddly, there seems to be no way to obtain battery status.  It's reported in HID Mode B, but not here.
-char* MogaBuildMsg(unsigned char code)
+int MogaSendMsg(MOGA_DATA *pMogaData, unsigned char code)
 {
 	uint8_t i, chksum = 0;
-	static char msg[SENDMSG_LEN];
-	msg[0] = 0x5a;          // identifier
-	msg[1] = SENDMSG_LEN;   // message length - always 5
-	msg[2] = code;          // command to send
-	msg[3] = MogaCID;       // controller id
+	char msg[SENDMSG_LEN];
+	msg[0] = 0x5a;           // identifier
+	msg[1] = SENDMSG_LEN;    // message length - always 5
+	msg[2] = code;           // command to send
+	msg[3] = pMogaData->CID;  // controller id
 	for (i = 0; i < SENDMSG_LEN-1; i++)
 		chksum = msg[i] ^ chksum;
 	msg[4] = chksum;
-	return msg;
+	send(pMogaData->Socket, msg, SENDMSG_LEN, 0);
+	return 1;
 }
 
 
@@ -144,68 +123,77 @@ char* MogaBuildMsg(unsigned char code)
 //   10 or 12    - 0x10 ..not sure what this means.  Could be identifying the kind of Moga.
 //   11 or 13    - checksum
 // If the message doesn't validate, something is messed up.  Just reset the connection.
-int MogaGetMsg(SOCKET s, unsigned char *recvbuf, int buf_len)
+int MogaGetMsg(MOGA_DATA *pMogaData)
 {
 	int retVal;
 	uint8_t i, chksum = 0, recvmsg_len;
-	if (buf_len != RECVBUF_LEN) return(-99);  // why would I ever do this
+	unsigned char recvbuf[RECVBUF_LEN];
 
 	// Returned data can be 12 or 14 bytes long, so the message length needs to be checked before a full read.
 	// We're never sending the codes to get a 12 byte response, but I'm leaving the check in anyway.
-	retVal = recv(s, (char *)recvbuf, 2, 0);
+	retVal = recv(pMogaData->Socket, (char *)recvbuf, 2, 0);
 	if (retVal == 2)
 	{
 		recvmsg_len = recvbuf[1];
-		retVal = recv(s, (char *)recvbuf+2, recvmsg_len-2, 0);
+		retVal = recv(pMogaData->Socket, (char *)recvbuf+2, recvmsg_len-2, 0);
 	}
-	else
-	{
-		printf("Moga disconnected.  (%d)\n", GetLastError());
-		return -1;
-	}
+	else   
+		return -1;    // Recv socket timeout
 	for (i = 0; i < recvmsg_len-1; i++)
 		chksum = recvbuf[i] ^ chksum;
-	if (recvbuf[0] != 0x7a || recvbuf[recvmsg_len-1] != chksum || recvbuf[2] != 101)
-	{
-		printf("Received bad data -  header %02x  chksum %02x  type %3d\n", recvbuf[0], recvbuf[recvmsg_len-1], recvbuf[2]);
-		printf("         expected -         7a           %02x       101\n", chksum);
-		return -2;
-	}
+	if (recvbuf[0] != 0x7a || recvbuf[recvmsg_len-1] != chksum)
+		return -2;    // Received bad data
+	memcpy(pMogaData->State, recvbuf+4, MOGABUF_LEN);
+	//PrintBuf(recvbuf);
 	return 1;
 }
 
 
-int MogaConnect()
+// Testing// Testing// Testing// Testing// Testing// Testing
+void PrintBuf(unsigned char *buf)
+{
+	int i;
+	SYSTEMTIME t;
+
+	GetSystemTime(&t);
+	printf ("[.%03d] ", t.wMilliseconds);
+	for (i=0; i<buf[1]; i++)
+		printf("%02x ", buf[i]);
+	printf("\n");
+}
+
+
+int MogaConnect(MOGA_DATA *pMogaData)
 {
 	SOCKADDR_BTH sockAddr;
-	DWORD timeout = 3000;
+	DWORD timeout = 2000;
 	int retVal;
 
-	MogaSocket = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
-	setsockopt(MogaSocket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(DWORD));
+	pMogaData->Socket = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
+	setsockopt(pMogaData->Socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(DWORD));
 	memset (&sockAddr, 0, sizeof(sockAddr));
 	sockAddr.addressFamily = AF_BTH;
 	sockAddr.serviceClassId = SerialPortServiceClass_UUID;
 	sockAddr.port = 0;  
-	sockAddr.btAddr = MogaAddr;
+	sockAddr.btAddr = pMogaData->Addr;
 
 	printf("\nAttempting connection ... ");
-	retVal = connect(MogaSocket, (SOCKADDR*)&sockAddr, sizeof(sockAddr));
+	retVal = connect(pMogaData->Socket, (SOCKADDR*)&sockAddr, sizeof(sockAddr));
 	if (retVal)
 	{
 		printf("Failed.  (%d)\n", GetLastError());
 		return 0;
 	}
 	printf("Success.  Press <Ctrl-C> to quit.\n");
-	send(MogaSocket, MogaBuildMsg(67), SENDMSG_LEN, 0);
+	MogaSendMsg(pMogaData, 67);
 	return 1;
 }
 
 
-void MogaReset()
+void MogaReset(MOGA_DATA *MogaData)
 {
-	closesocket(MogaSocket);
-	MogaSocket = INVALID_SOCKET;
+	closesocket(MogaData->Socket);
+	MogaData->Socket = INVALID_SOCKET;
 	if (KeepGoing)
 	{
 		printf("Reconnecting in 3 seconds.\n");
@@ -214,26 +202,37 @@ void MogaReset()
 }
 
 
-// Send the controller messages requesting its status as fast as we can get replies.
-// The Moga Power Pro response speed is 100 times a second, same as its message rate in HID mode.
-// Other controllers may be different?  A heartbeat timer here might be a good idea.
-int MogaPollStatus()
+// The Moga Power Pro response speed usually seems to be 100 times a second, same as its message rate in HID mode.
+// Occasionally the timestamps appear faster.  Not sure if this is due to my bluetooth adapter or something else.
+// The Moga seems to sometimes ignore command message 70 after a connection.
+// Not sure why, but sending it again a second later seems to work.
+void MogaListener(MOGA_DATA *pMogaData)
 {
-	int retVal;
-	unsigned char recvbuf[RECVBUF_LEN];
+	int retVal=1;
 
-	memset (recvbuf, 0, RECVBUF_LEN);
-	send(MogaSocket, MogaBuildMsg(69), SENDMSG_LEN, 0);
-	retVal = MogaGetMsg(MogaSocket, recvbuf, RECVBUF_LEN);
-	if (retVal == 1)
-		memcpy(MogaState, recvbuf+4, MOGABUF_LEN);
-	return retVal;
+	MogaSendMsg(pMogaData, 69);  // Get initial controller state
+	MogaSendMsg(pMogaData, 70);  // Enable listen mode
+	while (retVal == 1 && KeepGoing)
+	{
+		retVal = MogaGetMsg(pMogaData);
+		if (retVal < 1)
+		{
+			// Listen timeout.  Poll for status and to check connection state.
+			MogaSendMsg(pMogaData, 69);
+			retVal = MogaGetMsg(pMogaData);
+			if (retVal < 1)
+				printf("Moga disconnected.  (%d)\n", GetLastError());
+			else
+				MogaSendMsg(pMogaData, 70);  // Re-enable listen mode if the first try failed.
+		}
+		vJoyUpdate(pMogaData);
+	}
 }
 
 
 // The new 2.1.6 vJoy dll won't work with older versions of vJoy drivers.
 // Fortunately the old 2.0.5 vJoy dll works with new versions, despite giving an error message.
-int vJoySetup()
+int vJoySetup(UINT vJoyInterface)
 {
 	WORD VerDll, VerDrv;
 	if (!vJoyEnabled())
@@ -283,57 +282,45 @@ int vJoySetup()
 // Triggers are reported as both buttons and axis.
 // Thumbsticks report 00 at neutral, need to be corrected.
 // Buttons are so, so scrambled from what's "expected".
-void vJoyFeeder()
+void vJoyUpdate(MOGA_DATA *pMogaData)
 {
 	JOYSTICK_POSITION vJoyData;
 
-	vJoyData.bDevice = (BYTE)vJoyInterface;
+	vJoyData.bDevice = (BYTE)pMogaData->vJoyInt;
 
-	while (MogaPollStatus()==1 && KeepGoing)
+	vJoyData.lButtons = 0;
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 2) & 1) << 0);  // A
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 1) & 1) << 1);  // B
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 3) & 1) << 2);  // X
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 0) & 1) << 3);  // Y
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 6) & 1) << 4);  // L1
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 7) & 1) << 5);  // R1
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 5) & 1) << 6);  // Select
+	vJoyData.lButtons |= (((pMogaData->State[0] >> 4) & 1) << 7);  // Start
+	vJoyData.lButtons |= (((pMogaData->State[1] >> 6) & 1) << 8);  // L3
+	vJoyData.lButtons |= (((pMogaData->State[1] >> 7) & 1) << 9);  // R3
+	
+	switch((pMogaData->State[1] & 0x0F))
 	{
-		//for (int i=0; i<MOGABUF_LEN; i++)
-		//	printf("%02x ", MogaState[i]);
-		//printf("\n");
-
-		vJoyData.lButtons = 0;
-		vJoyData.lButtons |= (((MogaState[0] >> 2) & 1) << 0);  // A
-		vJoyData.lButtons |= (((MogaState[0] >> 1) & 1) << 1);  // B
-		vJoyData.lButtons |= (((MogaState[0] >> 3) & 1) << 2);  // X
-		vJoyData.lButtons |= (((MogaState[0] >> 0) & 1) << 3);  // Y
-		vJoyData.lButtons |= (((MogaState[0] >> 6) & 1) << 4);  // L1
-		vJoyData.lButtons |= (((MogaState[0] >> 7) & 1) << 5);  // R1
-		vJoyData.lButtons |= (((MogaState[0] >> 5) & 1) << 6);  // Select
-		vJoyData.lButtons |= (((MogaState[0] >> 4) & 1) << 7);  // Start
-		vJoyData.lButtons |= (((MogaState[1] >> 6) & 1) << 8);  // L3
-		vJoyData.lButtons |= (((MogaState[1] >> 7) & 1) << 9);  // R3
-		
-		switch((MogaState[1] & 0x0F))
-		{
-		case 0x01:  vJoyData.bHats = 0;     break;  // Hat N
-		case 0x09:  vJoyData.bHats = 4500;  break;  // Hat NE
-		case 0x08:  vJoyData.bHats = 9000;  break;  // Hat E
-		case 0x0A:  vJoyData.bHats = 13500; break;  // Hat SE
-		case 0x02:  vJoyData.bHats = 18000; break;  // Hat S
-		case 0x06:  vJoyData.bHats = 22500; break;  // Hat SW
-		case 0x04:  vJoyData.bHats = 27000; break;  // Hat W
-		case 0x05:  vJoyData.bHats = 31500; break;  // Hat NW
-		default:    vJoyData.bHats = -1;
-		}
-
-		FixAxis(MogaState[2]);
-		FixAxis(MogaState[3]);
-		FixAxis(MogaState[4]);
-		FixAxis(MogaState[5]);
-
-		vJoyData.wAxisX = MogaState[2] * 128;
-		vJoyData.wAxisY = (0xff - MogaState[3]) * 128;  //invert
-		vJoyData.wAxisXRot = MogaState[4] * 128;
-		vJoyData.wAxisYRot = (0xff - MogaState[5]) * 128;  //invert
-		vJoyData.wAxisZ = MogaState[6] * 128;
-		vJoyData.wAxisZRot = MogaState[7] * 128;
-		
-		UpdateVJD(vJoyInterface, (PVOID)&vJoyData);
+	case 0x01:  vJoyData.bHats = 0;     break;  // Hat N
+	case 0x09:  vJoyData.bHats = 4500;  break;  // Hat NE
+	case 0x08:  vJoyData.bHats = 9000;  break;  // Hat E
+	case 0x0A:  vJoyData.bHats = 13500; break;  // Hat SE
+	case 0x02:  vJoyData.bHats = 18000; break;  // Hat S
+	case 0x06:  vJoyData.bHats = 22500; break;  // Hat SW
+	case 0x04:  vJoyData.bHats = 27000; break;  // Hat W
+	case 0x05:  vJoyData.bHats = 31500; break;  // Hat NW
+	default:    vJoyData.bHats = -1;
 	}
+
+	vJoyData.wAxisX = FixAxis(pMogaData->State[2]) * 128;
+	vJoyData.wAxisY = (0xff - FixAxis(pMogaData->State[3])) * 128;  //invert
+	vJoyData.wAxisXRot = FixAxis(pMogaData->State[4]) * 128;
+	vJoyData.wAxisYRot = (0xff - FixAxis(pMogaData->State[5])) * 128;  //invert
+	vJoyData.wAxisZ = pMogaData->State[6] * 128;
+	vJoyData.wAxisZRot = pMogaData->State[7] * 128;
+		
+	UpdateVJD(pMogaData->vJoyInt, (PVOID)&vJoyData);
 }
 
 
@@ -347,14 +334,15 @@ int main(int argc, char **argv)
 {
 	WSADATA wsd;
 	int retVal;
+	MOGA_DATA MogaData = MOGA_NULL;
 
 	signal(SIGINT, intHandler);
 
-	printf("-----------------------------------------\n");
-	printf("  Moga serial to vJoy interface   v 0.9  \n");
-	printf("-----------------------------------------\n");
+	printf("-------------------------------------------\n");
+	printf("  Moga serial to vJoy interface   v 0.9.2  \n");
+	printf("-------------------------------------------\n");
 
-	retVal = vJoySetup();
+	retVal = vJoySetup(MogaData.vJoyInt);
 	if (retVal < 1)
 	{
 		Sleep(5000);
@@ -368,7 +356,7 @@ int main(int argc, char **argv)
 		exit(-5);
 	}
 
-	retVal = FindMogaAddress();
+	retVal = FindMogaAddress(&MogaData);
 	if (retVal < 1)
 	{
 		WSACleanup();
@@ -377,16 +365,16 @@ int main(int argc, char **argv)
 
 	while(KeepGoing)
 	{
-		retVal = MogaConnect();
+		retVal = MogaConnect(&MogaData);
 		if (retVal)
-			vJoyFeeder();
-		// vJoyFeeder only returns on an error.
-		MogaReset();
+			MogaListener(&MogaData);
+		// MogaListener only returns on an error.
+		MogaReset(&MogaData);
 	}
  
 	printf("\nExiting...\n");
 	WSACleanup();
-	RelinquishVJD(vJoyInterface);
+	RelinquishVJD(MogaData.vJoyInt);
 	Sleep(500);
 
 	return 0;
---
+++ MogaSerial/MogaSerial.h
@@ -0,0 +1,46 @@
+#ifndef _MOGASERIAL_H
+#define _MOGASERIAL_H
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+#include <winsock2.h>
+#include <ws2bth.h>
+#include "public.h"
+#include "vjoyinterface.h"
+ 
+typedef ULONGLONG BT_ADDR;
+
+#define     SENDMSG_LEN     5
+#define     RECVBUF_LEN   256
+#define     MOGABUF_LEN     8
+
+#define     FixAxis(c)      ((c<0x80)?(c+0x80):(c-0x80))
+
+BOOL		KeepGoing = true;
+
+// Moga connection/status info.
+struct MOGA_DATA {
+	UINT            vJoyInt;
+	BT_ADDR         Addr;
+	SOCKET          Socket;
+	unsigned char   CID;
+	unsigned char   State[MOGABUF_LEN];
+};
+const  MOGA_DATA	MOGA_NULL = { 1, 0, INVALID_SOCKET, 0x01, "" };
+
+
+BOOL	FindMogaAddress(MOGA_DATA *);
+int		MogaSendMsg(MOGA_DATA *, unsigned char);
+int		MogaGetMsg(MOGA_DATA *);
+int		MogaConnect(MOGA_DATA *);
+void	MogaReset(MOGA_DATA *);
+void	MogaListener(MOGA_DATA *);
+int		vJoySetup(UINT);
+void	vJoyUpdate(MOGA_DATA *);
+void	intHandler(int);
+
+void	PrintBuf(unsigned char *);
+
+#endif
